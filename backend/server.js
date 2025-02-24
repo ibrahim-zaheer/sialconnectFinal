fee597e8c7ce97b61ac9aaaa6d58f3bf8982c9a4
ibrahim-zaheer <zaheeribrahim346@gmail.com>
2025-02-24 12:26:33 +0500

    done

--- backend/server.js
+++ backend/server.js
@@ -204,6 +204,7 @@ const notificationRoutes = require("./routes/notification/notification");
 
 const { Server } = require("socket.io");
 
+const fs = require("fs");
 // Initialize dotenv to access environment variables
 dotenv.config();
 
@@ -220,6 +221,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 // ✅ Prefix all API routes with `/api` to avoid conflicts with frontend routes
 app.use("/api/auth", authRoutes);
