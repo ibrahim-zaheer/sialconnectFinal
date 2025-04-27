a4459c55b746006eed5488727cd1aeba6c4536f5
ibrahim-zaheer <zaheeribrahim346@gmail.com>
2025-04-27 14:19:42 +0500

    fix exporter offer page

--- frontend/src/components/profiles/ProfileContent.jsx
+++ frontend/src/components/profiles/ProfileContent.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { motion } from "framer-motion";
 import ProfileInfo from "./ProfileInfo";
 import SupplierOfferPage from "../../pages/offers/SupplierOfferPage";
+import ExporterOfferPage from "../../pages/offers/ExporterOfferPage";
 import PaymentListPage from "../../pages/payments/PaymentListPage";
 // import ReviewsSection from "../reviews/ReviewsBySupplier";
 import SupplierReviews from "../reviews/ReviewsBySupplier";
+import { useSelector } from "react-redux";
 
 const ProfileContent = ({ activeTab, user }) => {
+  const currentUser = useSelector((state) => state.user);
+  const userRole = currentUser?.role || user?.role;
   const renderContent = () => {
     switch (activeTab) {
       case "profile":
         return <ProfileInfo user={user} />;
+      // case "offers":
+      //   return <SupplierOfferPage/>;
       case "offers":
-        return <SupplierOfferPage/>;
+        // Show different offer pages based on role
+        return userRole === "exporter" ? (
+          <ExporterOfferPage />
+        ) : (
+          <SupplierOfferPage />
+        );
       case "payments":
         return <PaymentListPage/>;
       case "reviews":
