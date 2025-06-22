4227cd74573d3708d35afd592886e3f30ed9459e
ibrahim-zaheer <zaheeribrahim346@gmail.com>
2025-06-22 20:41:27 +0500

    done

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -1,5 +1,8 @@
+
+require('dotenv').config();
 const admin = require("firebase-admin");
-const serviceAccount = require("../sialconnect-firebase-adminsdk.json");
+// const serviceAccount = require("../sialconnect-firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
