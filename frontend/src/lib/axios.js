7c3123e1c6c60724e6500ced1d3c5a2c85194f66
ibrahim-zaheer <zaheeribrahim346@gmail.com>
2025-06-23 18:44:18 +0500

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
-    ? "" // Use relative URLs in production
+    ? "https://sialconnectfinal.onrender.com" // Use relative URLs in production
     : "https://sialconnectfinal.onrender.com"; // Use full URL in development
 
 export default axios;
