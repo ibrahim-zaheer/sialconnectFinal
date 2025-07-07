879713a68b0a944277c07a3a59cc4c5638425eda
ibrahim-zaheer <zaheeribrahim346@gmail.com>
2025-07-07 17:23:04 +0500

    done

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -88,8 +88,12 @@ import axios from "axios";
 import { useChatStore } from "./useChatStore"; 
 
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+// const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+
+const BASE_URL = import.meta.env.MODE === "development"
+  ? import.meta.env.VITE_API_URL_LOCAL
+  : import.meta.env.VITE_API_URL;
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
