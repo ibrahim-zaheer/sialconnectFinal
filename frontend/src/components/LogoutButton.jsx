089bce4a5c609f5dcb704b08a429af6e3c816c6a
alihassndev <alihassndev@gmail.com>
2025-07-07 19:11:41 +0500

    profile page and exporter product card

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -18,7 +18,7 @@ const LogoutButton = () => {
   return (
     <div className="flex justify-center items-center">
       <button
-        className="mt-2 border-1 px-5 py-2 text-[1rem] rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition-all duration-300"
+        className="mt-2 border-1 px-5 py-2 text-[1rem] rounded-lg bg-red-600 text-white hover:bg-red-500 transition-all duration-300"
         onClick={handleLogout}
       >
         Logout
--- frontend/src/components/profiles/ProfileInfo.jsx
+++ frontend/src/components/profiles/ProfileInfo.jsx
@@ -271,42 +271,44 @@ const ProfileInfo = () => {
                 )}
               </div>
 
-              <motion.button
-                variants={itemVariants}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                onClick={() => setShowProfileUpdateForm((prev) => !prev)}
-                className="flex items-center px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg transition-all shadow-md"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+              <div className="flex items-center gap-5">
+                {user.role === "supplier" && (
+                  <VerificationRequestButton
+                    className="px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-lg transition-all shadow-md"
+                    disabled={
+                      user.adminVerified === "pending" ||
+                      user.adminVerified === "approved"
+                    }
+                    onVerificationSubmitted={() => {
+                      dispatch(updateVerificationStatus({ status: "pending" }));
+                    }}
                   />
-                </svg>
-                Edit Profile
-              </motion.button>
+                )}
 
-              {user.role === "supplier" && (
-                <VerificationRequestButton
-                  className="px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-lg transition-all shadow-md"
-                  disabled={
-                    user.adminVerified === "pending" ||
-                    user.adminVerified === "approved"
-                  }
-                  onVerificationSubmitted={() => {
-                    dispatch(updateVerificationStatus({ status: "pending" }));
-                  }}
-                />
-              )}
+                <motion.button
+                  variants={itemVariants}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  onClick={() => setShowProfileUpdateForm((prev) => !prev)}
+                  className="flex items-center px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg transition-all shadow-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                    />
+                  </svg>
+                  Edit Profile
+                </motion.button>
+              </div>
             </div>
 
             {/* Profile Update Form */}
